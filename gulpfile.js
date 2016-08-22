98c4ae9dac5a92623a54db8dba1a4a326baff9bf
Hamish Gray <hamishgray@Hamishs-MacBook-Pro.local>
2016-08-22 15:56:20 +0100

    Updated links, fixed offer section anchor links

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ gulp.task('serve', function () {
       server: {
         baseDir: '_site/',
         routes: {
-          '/brittanyferries': '_site/'
+          '/golf-in-france': '_site/'
         }
       }
     });
